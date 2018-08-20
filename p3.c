0683c299824453096652f8be12d7c86f75c53250
Ashwin Nayak U <31366763+not4win@users.noreply.github.com>
2018-08-20 11:24:30 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <omp.h>
+int main()
+{
+int x=0;
+	#pragma omp parallel shared(x)
+	{
+		int tid=omp_get_thread_num();
+		x=x+1;
+		printf("Thread %d has tid value of %d \n",tid,x);
+	}
+}
